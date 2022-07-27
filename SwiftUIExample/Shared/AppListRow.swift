3d2211535dc6ba7b7520e163ca3ee6418dd09463
Taeyoung Son <taeyoungson10@gmail.com>
2022-07-27 17:08:00 +0900

    #dev AppListRow 생성

---
+++ SwiftUIExample/Shared/AppListRow.swift
@@ -0,0 +1,20 @@
+//
+//  AppListRow.swift
+//  SwiftUIExample
+//
+//  Created by Taeyoung Son on 2022/07/27.
+//
+
+import SwiftUI
+
+struct AppListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        AppListRow()
+    }
+}
