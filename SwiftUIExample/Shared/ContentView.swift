7c610baf2066c533180bc4e7266de2fb55613f12
Taeyoung Son <taeyoungson10@gmail.com>
2022-08-01 19:01:41 +0900

    #dev HStack 사용해보기

--- SwiftUIExample/Shared/ContentView.swift
+++ SwiftUIExample/Shared/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Text("Hello, world!")
+                .padding()
+            Text("Hello, world!")
+                .padding()
+        }
     }
 }
 
