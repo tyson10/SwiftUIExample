dc9f19b8a44e1e2c4e600aa948f6c8f74f29b607
Taeyoung Son <taeyoungson10@gmail.com>
2022-08-05 18:27:51 +0900

    #dev AppListRow 구현중(코드 정리 필요)

--- SwiftUIExample/Shared/AppListRow.swift
+++ SwiftUIExample/Shared/AppListRow.swift
@@ -7,11 +7,7 @@
 
 import SwiftUI
 
-struct AppListRow: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+
 
 struct AppListRow_Previews: PreviewProvider {
     static var previews: some View {
