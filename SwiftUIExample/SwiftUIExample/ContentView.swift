647646c45d39c80386656ea28141d1dee805e54e
Taeyoung Son <taeyoungson10@gmail.com>
2022-08-06 18:20:12 +0900

    #dev 앱 리스트 화면 구현

--- SwiftUIExample/SwiftUIExample/ContentView.swift
+++ SwiftUIExample/SwiftUIExample/ContentView.swift
@@ -9,13 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                AppListRow()
+                AppListRow()
+                AppListRow()
+                AppListRow()
+                AppListRow()
+                AppListRow()
+            }
+            .navigationTitle("앱 스토어")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AppListRow: View {
+    var body: some View {
+        HStack {
+            Image("AppImage")
+                .resizable()
+                .frame(width: 60, height: 60, alignment: .center)
+            
+            VStack(alignment: .leading) {
+                Text("앱 이름")
+                    .font(.system(size: 17))
+                
+                Text("앱이 뭔지 알려드림")
+                    .font(.system(size: 12))
+            }
+            Spacer()
+            VStack(alignment: .center) {
+                Button("받기", action: {})
+                    .frame(width: 70, height: nil, alignment: .center)
+                    .background(.brown)
+                    .cornerRadius(100)
+                    
+                Text("인앱 결제")
+                    .font(.system(size: 12))
+            }
+        }
     }
 }
