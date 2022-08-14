78989fea4323bc9071b9c11fef5b0983270a12ae
Taeyoung Son <taeyoungson10@gmail.com>
2022-08-14 18:47:34 +0900

    #dev AppDetailView 생성

---
+++ SwiftUIExample/SwiftUIExample/UI/AppDetail/AppDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AppDetailView.swift
+//  SwiftUIExample
+//
+//  Created by Taeyoung Son on 2022/08/14.
+//
+
+import SwiftUI
+
+struct AppDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppDetailView()
+    }
+}
