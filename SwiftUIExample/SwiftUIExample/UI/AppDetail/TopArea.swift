76ecf5697d8839ddcef874505ad34e0053cf2fc0
Taeyoung Son <taeyoungson10@gmail.com>
2022-08-15 22:50:14 +0900

    #dev TopArea 추가

---
+++ SwiftUIExample/SwiftUIExample/UI/AppDetail/TopArea.swift
@@ -0,0 +1,20 @@
+//
+//  TopArea.swift
+//  SwiftUIExample
+//
+//  Created by Taeyoung Son on 2022/08/15.
+//
+
+import SwiftUI
+
+struct TopArea: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopArea_Previews: PreviewProvider {
+    static var previews: some View {
+        TopArea()
+    }
+}
