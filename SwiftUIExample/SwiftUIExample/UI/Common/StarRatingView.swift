5fb6283f1af767020310d69e67f9123a94fcf781
Taeyoung Son <taeyoungson10@gmail.com>
2022-08-27 00:02:12 +0900

    #dev StarRatingView 생성

---
+++ SwiftUIExample/SwiftUIExample/UI/Common/StarRatingView.swift
@@ -0,0 +1,20 @@
+//
+//  StarRatingView.swift
+//  SwiftUIExample
+//
+//  Created by Taeyoung Son on 2022/08/27.
+//
+
+import SwiftUI
+
+struct StarRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StarRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        StarRatingView()
+    }
+}
